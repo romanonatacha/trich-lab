59ca615420a1ab5f7d2e6dce6bea6d13b522c2f4
Natacha Romano <romano.n@hotmail.com>
2018-08-04 23:12:15 -0300

    fix collapse menu responsivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { BsDropdownModule } from 'ngx-bootstrap';
+import { BsDropdownModule, CollapseModule } from 'ngx-bootstrap';
 import { CoverComponent } from './cover/cover.component';
+import { AboutComponent } from './about/about.component';
+import { CollapseComponent } from './collapse/collapse.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CoverComponent
+    CoverComponent,
+    AboutComponent,
+    CollapseComponent
   ],
   imports: [
     BrowserModule,
     BsDropdownModule,
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
