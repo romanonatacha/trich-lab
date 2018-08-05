18f2086b19d328d475cb07e258160f356f49569a
Natacha Romano <romano.n@hotmail.com>
2018-08-04 21:22:14 -0300

    header e cover

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BsDropdownModule } from 'ngx-bootstrap';
+import { CoverComponent } from './cover/cover.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CoverComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BsDropdownModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
